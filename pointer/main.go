fc7a4ce2dbda0dc0093cb89f17825defac29b325
Nan Oo Hlaing <nanoohlaing237@gmail.com>
2022-11-25 09:28:44 +0630

    new chapter

--- pointer/main.go
+++ pointer/main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+func zeroptr(iptr *int) {
+	*iptr = 132312321
+}
+
 func main() {
-	fmt.Println("testing")
+	num := 50
+
+	var pointer = &num
+
+	fmt.Println("Pointer is ", pointer)
+	fmt.Println("Value is ", *pointer)
+
+	*pointer = *pointer + 2
+	fmt.Println("Additional Value is ", num)
+
+	i := 1
+	zeroptr(&i)
+	fmt.Println("Next Test ", i)
+
 }
